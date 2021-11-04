5977a2685ba1f4c8b43529bed8e985f88fcead10
Jelly_Pudding <samwalters11111@hotmail.com>
2021-11-04 19:00:09 +0000

    get row and column input from user

--- gui.py
+++ gui.py
@@ -1,34 +1,41 @@
 from pathfinder import *
 import pygame
 
-
-print(maze[6][6])
-print(row)
-print(column)
-
-
-
+#default size of row and column
+num_of_rows = 10
+num_of_cols = 10
 
 screen = pygame.display.set_mode((200, 200))
 pygame.font.init()
 pygame.display.set_caption('A*')
 myfont = pygame.font.SysFont("timesnewroman", 30)
 text = myfont.render("start game", True, (255,255,255))
-input_box1 = pygame.Rect(39, 70, 140, 32)
+input_box1 = pygame.Rect(30, 45, 150, 32)
+input_box2 = pygame.Rect(30, 115, 150, 32)
 color_inactive = pygame.Color('lightskyblue3')
 color_active = pygame.Color('dodgerblue2')
-active = False
+active1 = False
+active2 = False
 input_text = ""
+input_text2 = ""
 
 def displayer():
-    textsurface = myfont.render('Enter Row:', False, (65, 0, 0))
-    screen.blit(textsurface,(40,30))
-    if active == True:
+    textsurface = myfont.render("Enter Row#:", False, (90, 0, 0))
+    screen.blit(textsurface,(30,10))
+    secondquestion = myfont.render("Enter Column#:", False, (90, 0, 0))
+    screen.blit(secondquestion,(7,80))
+    if active1 == True:
         pygame.draw.rect(screen, color_active, input_box1)
     else:
         pygame.draw.rect(screen, color_inactive, input_box1)
-    second_text = myfont.render(input_text, True, (50, 0, 0))
-    screen.blit(second_text, (39,70))
+    if active2 == True:
+        pygame.draw.rect(screen, color_active, input_box2)
+    else:
+        pygame.draw.rect(screen, color_inactive, input_box2)        
+    create_input_text_1 = myfont.render(input_text, True, (50, 0, 0))
+    screen.blit(create_input_text_1, (39,43))
+    create_input_text_2 = myfont.render(input_text2, True, (50, 0, 0))
+    screen.blit(create_input_text_2, (39, 115))
     pygame.display.update()
 
 running = True
@@ -40,19 +47,36 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if input_box1.collidepoint(event.pos):
-                active = not active
+                active1 = not active1
             else:
-                active = False
-            color = color_active if active else color_inactive
+                active1 = False
+            color = color_active if active1 else color_inactive
+            if input_box2.collidepoint(event.pos):
+                active2 = not active1
+            else:
+                active2 = False
+            color = color_active if active2 else color_inactive
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 running = False
-            if active == True:
+            if active1 == True:
                 if event.key == pygame.K_RETURN:
                     num_of_rows = input_text
                 elif event.key == pygame.K_BACKSPACE:
                     input_text = input_text[:-1]
+                    num_of_rows = input_text
                 else:
                     input_text += event.unicode
+                    num_of_rows = input_text
+            if active2 == True:
+                if event.key == pygame.K_RETURN:
+                    num_of_cols = input_text2
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text2[:-1]
+                    num_of_rows = input_text2
+                else:
+                    input_text2 += event.unicode
+                    num_of_rows = input_text2
 
 print(num_of_rows)
+print(num_of_cols)
