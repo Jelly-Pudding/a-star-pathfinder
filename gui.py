4a56d29ddb5a3b8a380bde82c3c1c915c48344b1
Jelly_Pudding <samwalters11111@hotmail.com>
2021-11-05 00:00:26 +0000

    accept user input

--- gui.py
+++ gui.py
@@ -104,11 +104,26 @@ screen = pygame.display.set_mode((num_of_rows * 20, num_of_cols * 20))
 print(pygame.display.get_window_size())
 #each box will be 5x5
 
+input_box_list = []
+for i in range(len(maze)):
+    for j in range(len(maze[i])):
+        input_box_list.append({"rectangle": pygame.Rect(20*i, 20*j, 20, 20), "coordinates": [j, i]})
+
+print(input_box_list)
+
+input_box2 = pygame.Rect(30, 115, 150, 32)
+
 def game_display():
     for i in range(len(maze)):
         for j in range(len(maze[i])):
-            pygame.draw.rect(screen, (0, 255, 0), (20*i, 20*j, 20, 20))
-
+            if maze[j][i] == 3:
+                pygame.draw.rect(screen, (0,255,0), pygame.Rect(20*i, 20*j, 20, 20))
+            else:
+                pygame.draw.rect(screen, (40,40,40), pygame.Rect(20*i, 20*j, 20, 20))
+    """
+    for index_of_item in range(len(input_box_list)):
+        pygame.draw.rect(screen, (40,40,40), input_box_list[index_of_item]["rectangle"])
+    """
     for i in range(len(maze)):
         pygame.draw.line(screen, (255, 255, 255), (i*20, 0), (i*20, num_of_cols*20), 2)
         for j in range(len(maze[i])):
@@ -121,42 +136,6 @@ def game_display():
     #line down the right hand side
 
     pygame.draw.line(screen, (255, 255, 255), (num_of_rows*20, 0), (num_of_rows*20, num_of_cols*20), 4)
-
-    '''
-    pygame.draw.line(screen, (255, 255, 255), (20, 0), (20, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (40, 0), (40, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (60, 0), (60, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (80, 0), (80, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (100, 0), (100, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (120, 0), (120, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (140, 0), (140, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (160, 0), (160, 200), 2)
-    pygame.draw.line(screen, (255, 255, 255), (180, 0), (180, 200), 2)
-
-
-    pygame.draw.line(screen, (255, 255, 255), (0, 20), (200, 20), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 40), (200, 40), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 60), (200, 60), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 80), (200, 80), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 100), (200, 100), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 120), (200, 120), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 140), (200, 140), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 160), (200, 160), 2)
-    pygame.draw.line(screen, (255, 255, 255), (0, 180), (200, 180), 2)
-    '''
-    '''
-    pygame.draw.rect(screen, (0, 255, 0), (0, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (20, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (40, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (60, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (80, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (100, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (120, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (140, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (160, 0, 20, 20))
-    pygame.draw.rect(screen, (0, 255, 0), (180, 0, 20, 20))
-    '''
-        
 
     pygame.display.update()
 
@@ -172,4 +151,12 @@ while running:
             if event.key == pygame.K_q:
                 pygame.quit()
                 running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for idx in range(len(input_box_list)):
+                if input_box_list[idx]["rectangle"].collidepoint(event.pos):
+                    print(idx)
+                    print(input_box_list[idx]["coordinates"])
+                    maze[input_box_list[idx]["coordinates"][0]][input_box_list[idx]["coordinates"][1]] = 3
+
+printer(maze)
 
